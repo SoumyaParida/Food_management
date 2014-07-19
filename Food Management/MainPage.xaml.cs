aa06789fcfb939e6149067e354bc0df264c0e9de
SoumyaParida <soumya.parida3@gmail.com>
2014-07-19 15:02:21 +0200

    added teh files to food management repository

---
+++ Food Management/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Food_Management
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+            /*List<Items> catagory = new List<Items>();
+            catagory.Add(new Items() { Name = "Fruits" });
+            catagory.Add(new Items() { Name = "Vegitables" });
+            catagory.Add(new Items() { Name = "Milk Products" });
+            catagory.Add(new Items() { Name = "Bread" });
+            catagory.Add(new Items() { Name = "Meat Products" });
+            catagory.Add(new Items() { Name = "Juice" });
+            catagory.Add(new Items() { Name = "Snacks" });
+            this.FoodCatagory.ItemsSource = catagory;*/
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string FoodName = "";
+            if (AddFood.Text == "Food Name")
+            {
+                MessageBox.Show("Please Enter the food name");
+                // NavigationService.Navigate(new Uri("/MainPage.xaml?" + FoodName, UriKind.Relative));
+            }
+            else
+            {
+                FoodName = AddFood.Text;
+                NavigationService.Navigate(new Uri("/Calender.xaml?msg=" + FoodName, UriKind.Relative));
+            }
+        }
+
+        private void Catagory_list(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Catagory.xaml?", UriKind.Relative));
+        }
+    }
+}
---
+++ Food Management/items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Items
+{
+    public string Name
+    {
+        get;
+        set;
+    }
+    public string typeValue
+    {
+        get;
+        set;
+    }
+
+}
